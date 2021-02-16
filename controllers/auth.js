b06b999d74e273035636153bb7c73a3538e4a05d
Prince Narteh <prinarteh@gmail.com>
2021-02-16 16:40:59 +0000

    Add Route Protection

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto');
 const User = require('../models/user');
 const ErrorResponse = require('../utils/errorResponse');
+const sendEmail = require('../utils/sendEmail');
 
 const sentToken = (user, statusCode, res) => {
     const token = user.getSignedToken();
@@ -26,7 +28,6 @@ exports.login = async (req, res, next) => {
 
     try {
         const user = await User.findOne({ email }).select('+password');
-
         if (!user || !(await user.comparePasswords(password))) {
             return next(new ErrorResponse('Invalid Credentials.', 401));
         }
@@ -37,5 +38,69 @@ exports.login = async (req, res, next) => {
             status: false,
             error: error.message
         })
+    }
+}
+
+exports.forgotPassword = async (req, res, next) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return next(new ErrorResponse('Email could not be sent', 404));
+        }
+
+        const resetToken = user.getResetPasswordToken();
+        await user.save();
+
+        const resetURL = `http://localhost:3000/passwordreset/${resetToken}`
+        const message = `
+        <h1>You have requested a password reset.</h1>
+        <p>Please go to the link to reset your password.</p>
+        <a href=${resetURL} clicktracking=off>${resetURL}</a>
+        `
+        try {
+            sendEmail({
+                to: user.email,
+                subject: "Password Reset Request",
+                html: message
+            })
+
+            res.status(200).json({ success: true, data: "Email sent." })
+        } catch (error) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+
+            await user.save();
+            return next(new ErrorResponse('Email could not be send.', 500));
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.resetPassword = async (req, res, next) => {
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resetToken).digest('hex');
+
+    try {
+        const user = await User.findOne({
+            resetPasswordToken,
+            resetPasswordExpire: { $gt: Date.now() }
+        })
+
+        if (!user) {
+            return next(new ErrorResponse('Invalid Reset Token', 400));
+        }
+
+        user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save();
+
+        res.status(201).json({ success: true, data: "Password Reset Success." });
+    } catch (error) {
+        next(error);
     }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,7 @@ const authController = require('../controllers/auth');
 
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
+router.route('/forgotpassword').post(authController.forgotPassword);
+router.route('/resetpassword/:resetToken').put(authController.resetPassword);
 
 module.exports = router;
